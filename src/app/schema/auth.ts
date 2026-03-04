d2ec20a447d3cc885951ec79d309980edeb51a1a
SameerJangid <jangidsameer77@gmail.com>
2026-03-04 22:40:25 +0530

    refactor: standardize formatting and validation in auth schemas

--- src/app/schema/auth.ts
+++ src/app/schema/auth.ts
@@ -1,12 +1,21 @@
 import z from "zod";
 export const SignUpSchema = z.object({
-  email: z.email(),
-  password: z.string().min(6).max(20),
-  name: z.string().min(2).max(50),
+  name: z
+    .string()
+    .min(1, "Enter full name")
+    .min(2, "Full name must be at least 2 characters"),
+  email: z.string().min(1, "Enter email").email("Enter valid email"),
+  password: z
+    .string()
+    .min(1, "Enter password")
+    .min(6, "Password must be at least 6 characters"),
 });
 
 // login only requires email and password
 export const LoginSchema = z.object({
-  email: z.email(),
-  password: z.string().min(6).max(20),
+  email: z.string().min(1, "Enter email").email("Enter valid email"),
+  password: z
+    .string()
+    .min(1, "Enter password")
+    .min(6, "Password must be at least 6 characters"),
 });
